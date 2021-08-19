5ec49113490aabac30e4c60420263ea8fe64fa68
CISSE Fatoumata <fatou.ycisse@gmail.com>
2021-08-19 14:47:49 +0200

    router.js

---
+++ backend/routes/user.js
@@ -0,0 +1,10 @@
+//importations
+const express = require('express');
+const router = express.Router();
+const userCtrl = require('../controllers/user');
+
+//routes
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
+
+module.exports = router;
